26f700ff710c9901c0841a30b7ab865ea8db09c4
Santhu1243 <santhosh.sk1243@gmail.com>
2025-01-24 23:48:49 +0530

    comments updated

--- chezzionwebapp/chezzionwebapp/settings.py
+++ chezzionwebapp/chezzionwebapp/settings.py
@@ -151,12 +151,14 @@ LOGOUT_REDIRECT_URL = 'nonsap:dashboard'
 
 # Email settings for sending emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.yourmailprovider.com'  # SMTP server address
+EMAIL_HOST = 'smtp.gmail.com'  # SMTP server address
 EMAIL_PORT = 587  # SMTP port (use 465 for SSL, 587 for TLS)
-EMAIL_USE_TLS = True  # Use TLS (True/False)
-EMAIL_HOST_USER = 'youremail@example.com'  # Your email address
-EMAIL_HOST_PASSWORD = 'yourpassword'  # Your email password
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'chezzionweb@gmail.com'  # Your email address
+EMAIL_HOST_PASSWORD = 'bxnn ugps gdck smdw'  # Your email password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  # Default sender email address
+
+
 
 
 import os
