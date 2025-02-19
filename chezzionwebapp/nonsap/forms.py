423cbd11b28b6cb27e3acf815b6905c56f36aac4
Santhu1243 <santhosh.sk1243@gmail.com>
2025-02-19 10:09:37 +0530

    status time and priority update

--- chezzionwebapp/nonsap/forms.py
+++ chezzionwebapp/nonsap/forms.py
@@ -99,3 +99,27 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = '__all__'  # Adjust fields as necessary
+
+
+
+class IssueUploadForm(forms.Form):
+    csv_file = forms.FileField()
+
+class ChangePasswordForm(forms.ModelForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    new_password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
+
+    class Meta:
+        model = User
+        fields = ['old_password', 'new_password', 'confirm_password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if new_password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+        return cleaned_data
+
+
