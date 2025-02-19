dbf693a1f446f8f61f90a5693e4e33ba246570da
root <root@srv701318.hstgr.cloud>
2025-02-19 12:41:47 +0000

    new

--- chezzionwebapp/chezzionwebapp/settings.py
+++ chezzionwebapp/chezzionwebapp/settings.py
@@ -161,7 +161,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'chezzionweb@gmail.com'  
-EMAIL_HOST_PASSWORD = 'bxnn ugps gdck smdw'  
+EMAIL_HOST_PASSWORD = 'bxnn ugps gdck smdw'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  #
 
 
